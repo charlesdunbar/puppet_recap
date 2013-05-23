c3db42d3c72d217a7edee3fbaae458682c8718a6
Charles Dunbar <cdunbar@puppetlabs.com>
2013-05-23 14:38:39 -0700

    Fixes to ensure actually runs

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,14 +2,15 @@ class puppet_recap::install {
 
   file { '/tmp/recap.tar.gz':
     ensure => present,
-    source => 'puppet:///puppet_recap/recap.tar.gz',
+    source => 'puppet:///modules/puppet_recap/recap.tar.gz',
   }
 
   exec { 'untar_recap':
-    creates => "/tmp/recap.tar.gz",
     cwd     => '/tmp',
     path    => ['/bin','/usr/bin','/usr/sbin'],
     command => 'tar -zxf recap.tar.gz',
+    require => File['/tmp/recap.tar.gz'],
+    creates => "/var/log/recap",
   }
 
   exec { 'install_recap':
@@ -17,7 +18,7 @@ class puppet_recap::install {
     cwd      => '/tmp/recap',
     command  => 'bash recap-installer',
     path     => ['/bin','/usr/bin','usr/sbin'],
-    requires => Exec['untar_recap'],
+    require => Exec['untar_recap'],
   }
 
 }
